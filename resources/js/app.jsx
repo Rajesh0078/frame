deffa33d0164b3cd984569dc7bbabbd9a451e89c
Rajesh Peketi <122203953+Rajesh0078@users.noreply.github.com>
2025-03-20 18:06:01 +0530

    Update app.jsx

--- resources/js/app.jsx
+++ resources/js/app.jsx
@@ -2,6 +2,7 @@ import { createInertiaApp } from '@inertiajs/react'
 import { createRoot } from 'react-dom/client'
 
 createInertiaApp({
+    title: name => name ? `${name} | Frame Up - Frame your imagination` : "Frame Up - Frame your imagination",
     resolve: name => {
         const pages = import.meta.glob('./Pages/**/*.jsx', { eager: true })
         return pages[`./Pages/${name}.jsx`]
